0239298965da19fca78c8cd7f78546e6452cd44b
tanghieuthanh221027-arch <tanghieuthanh221027@gmail.com>
2025-10-23 13:14:47 +0700

    Change variable type from int to float

--- bài 9 ss3 .c
+++ bài 9 ss3 .c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include <math.h>
 int main (){
-	int n ;
+	float n ;
 	printf("nhap n :") ;
-	scanf("%d" , &n) ;
+	scanf("%f" , &n) ;
 	float result = 1/((n - 1) * n) + 1/(n * (n + 1)) + 1/((n + 1) * (n + 2)) ;
 	printf("%.2f" , result) ;
 	return 0 ; 
 }
+

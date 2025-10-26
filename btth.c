f88c7f5c67f9c4aa352d732059f10d9de88a4ef4
tanghieuthanh221027-arch <tanghieuthanh221027@gmail.com>
2025-10-26 14:47:07 +0700

    Update btth.c

--- btth.c
+++ btth.c
@@ -6,9 +6,10 @@ int main () {
 	scanf("%f" , &a ) ;
 	printf("Nhap b :") ;
 	scanf("%f" , &b ) ;
-	float result =sqrt( pow(a,2) + pow(b,2) ) /(a + b) + sqrt(a) + sqrt(b) )/(a * b);
+	float result =(sqrt( pow(a,2) + pow(b,2) )) /(a + b) + (sqrt(a) + sqrt(b)) /(a * b);
 	printf("%.2f" , result);
 	return 0 ;
 }
+
 
 
